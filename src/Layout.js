4abf497d67a5d3acf7902cff98337200a46c64f3
Jose Mendoza <Pball626@gmail.com>
2024-02-19 14:54:42 -0600

    components folder added and layout

---
+++ src/Layout.js
@@ -0,0 +1,13 @@
+import {Outlet, outlet} from "react-router-dom"
+
+import React from 'react'
+
+const Layout = () => {
+    return (
+        <main>
+            <Outlet/>
+        </main>
+    )
+}
+
+export default Layout
